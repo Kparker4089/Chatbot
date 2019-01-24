222e056d7e35b68eb9a805e7dd13827b64f15b96
unknown <kparker4089@myhta.org>
2019-01-24 13:09:10 -1000

    added app.js

--- webdev2/app.js
+++ webdev2/app.js
@@ -1,1 +1,31 @@
-console.log("test");
+function getValue(){
+	var user = document.getElementById("chatBox");
+	var userMessage = user.value;
+
+	
+
+	var user2 = document.getElementById("btn");
+	user2.addEventListener("click", getValue);
+
+	var chatMsg = document.getElementById("Question");
+
+
+	
+	 if(userMessage === "Yes, I am a student"){
+		chatMsg.innerHTML = "Do you have your Identification Card?";
+	} 
+  
+  else if(userMessage ==="No, I don't have my ID card"){
+		chatMsg.innerHTML = "Here take a temporary ID for the day.";
+	} 
+  
+  else if(userMessage === "No"){
+		chatMsg.innerHTML = "Ok then, Move along";
+	} 
+  
+else if(userMessage === "Yes, I have my ID card"){
+	chatMsg.innerHTML = "Good job, now get to class."
+}
+}
+
+getValue();
